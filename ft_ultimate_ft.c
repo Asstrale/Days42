0621c05de74bda8f6e9132f8512b7d60d179840a
Asstrale <87241181+Asstrale@users.noreply.github.com>
2024-08-11 15:50:28 +0200

    Update ft_ultimate_ft.c
    V2

--- ft_ultimate_ft.c
+++ ft_ultimate_ft.c
@@ -1,40 +1,39 @@
 #include <stdio.h>
 
-void ft_ultimate_ft(int *********nbr)
+void ft_ultimate_ft(int ********nbr)
 {
-     *********nbr = 42;
+    *nbr = 42;  
 }
 
 int main()
 {
-    int c = 0;
- 
+    int nbr = 1;
+    printf("%d %p\n", nbr, &nbr);
     
-    int *nbr2 = &c;
-    printf("%p %d\n",&c,c); 
+    int *nbr1 = &nbr;
+    printf("%p %p\n", &nbr, nbr1);
     
-    int **nbr3 = &nbr2;
-    printf("%p %p\n",&nbr2,&c); 
+    int *nbr2 = &nbr1;
+    printf("%p %p\n", nbr1, nbr2);
     
-    int ***nbr4 = &nbr3;
-    printf("%p %p\n",&nbr3,&nbr2); 
+    int *nbr3 = &nbr2;
+    printf("%p %p\n", nbr2, nbr3);
     
-    int ****nbr5 = &nbr4;
-    printf("%p %p\n",&nbr4,&nbr3); 
-    int *****nbr6 = &nbr5;
+    int *nbr4 = &nbr3;
+    printf("%p %p\n", nbr3, nbr4);
     
-    printf("%p %p\n",&nbr5, &nbr4); 
-    int ******nbr7 = &nbr6;
+    int *nbr5 = &nbr4;
+    printf("%p %p\n", nbr4, nbr5);
     
-    printf("%p %p\n",&nbr6, &nbr5); 
-    int *******nbr8 = &nbr7;
+    int *nbr6 = &nbr5;
+    printf("%p %p\n", nbr5, nbr6);
     
-    printf("%p %p\n",&nbr7, &nbr8); 
-    int ********nbr9 = &nbr8;
+    int *nbr7 = &nbr6;
+    printf("%p %p\n", nbr6, nbr7);
     
-    printf("%p %p\n",&nbr8, &nbr7); 
-    int *********nbr10 = &nbr9;
-
-    ft_ultimate_ft(nbr10);
-    printf("%d \n",c); 
+    int *nbr8 = &nbr7;
+    printf("%p %p\n", nbr7, nbr8);
+    
+    ft_ultimate_ft(&nbr);
+    printf("%p %d\n", nbr8, nbr);
 }
